8dec4208daf61a3e44d0770285bdb1fd43419169
neeldarji12 <neeldarji1431@gmail.com>
2026-09-26 05:17:21 +0530

    Revert "frontend"

--- src/components/StatusBadge.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { statusClass } from "../utils/leaveUtils";
-
-export default function StatusBadge({ status }) {
-  return <span className={`status-badge ${statusClass(status)}`}>{status}</span>;
-}
